1c0af34f3e40910c20b747cccd5e3ad0878345bf
Naïm Guénette-Harvey <2360188@cegepmontpetit.ca>
2026-09-01 17:38:44 -0400

    FCT: Début de platform

--- Models/Game.cs
+++ Models/Game.cs
@@ -5,16 +5,17 @@ using RelationsNaN.Models;
 namespace RelationsNaN.Models
 {
     public class Game
-{
-    public int Id { get; set; }
-    public string Name { get; set; }
-    public string Image { get; set; }
-    public int ReleaseYear { get; set; }
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Image { get; set; }
+        public int ReleaseYear { get; set; }
 
-    public int? GenreId { get; set; }
-    [ValidateNever]
-    public Genre? Genre { get; set; }
+        public int? GenreId { get; set; }
+        [ValidateNever]
+        public Genre? Genre { get; set; }
 
-
-}
+        [ValidateNever]
+        public List<Platform>? Platforms { get; set; }
+    }
 }
